437af12845b13af7fcfb40d26f78865f01d2c671
Ayush Srivastava <ayushforbussiness2924@gmail.com>
2026-06-02 02:33:13 +0530

    More about `sizeof()` data

--- ftda.c
+++ ftda.c
@@ -8,7 +8,9 @@ int main(void){
     double average = 0.0; // Good practice
     printf("a = %d, b = %d, c = %d",a,b,c);
     average = (a + b + c) / 3.0; // Conversion if 3
-    printf("Average = %lf\n", average);
+    printf("Average = %lf\n", average); 
+    printf("On my System\n"); // Printing sizes of diff data types on my system
+    printf("Size of int %lu \nSize Of long int %lu \nSize of Char is %lu \nSize of Float is %lu \nSize of Double %lu \nSize of long Double %lu", sizeof(int),sizeof(long int), sizeof(char), sizeof(float), sizeof(double), sizeof(long double));
     return 0;
 }
 
